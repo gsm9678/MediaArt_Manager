d617d76f756abfa8cc8e0fba9985861aafb56280
gsm9678 <32432674+gsm9678@users.noreply.github.com>
2025-12-03 18:58:46 +0900

    Rename OscSender to OscContentPlayer
    Renamed OscSender.cs and its class to OscContentPlayer.cs/OscContentPlayer for improved clarity and alignment with its functionality.

--- Assets/02.Scripts/Osc/OscContentPlayer.cs
+++ Assets/02.Scripts/Osc/OscContentPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OscSender : MonoBehaviour
+public class OscContentPlayer : MonoBehaviour
 {
     private List<ContentsAddressLine> contentSequence;
     Coroutine coroutine;
