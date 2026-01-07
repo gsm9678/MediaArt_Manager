c8cdb0e52ef873890e62f5703ab4d2439bb8ff3d
gsm9678 <32432674+gsm9678@users.noreply.github.com>
2026-01-07 19:13:08 +0900

    Update ManagerScene UI layout and add images
    Refactored UI elements in ManagerScene.unity, including changes to RectTransforms, text labels, and the addition of new UI components such as placeholders and dropdowns. Added new image assets and their meta files. Updated database.json and modified WindowOpenView.cs to support these UI changes.

--- Assets/02.Scripts/UI/WindowOpenView.cs
+++ Assets/02.Scripts/UI/WindowOpenView.cs
@@ -3,40 +3,56 @@ using UnityEngine.UI;
 
 public class WindowOpenView : MonoBehaviour
 {
-    [Header("Content Management")]
-    [SerializeField] private Button ContentManagementWindowButton;
-    [SerializeField] private GameObject ContentManagementWindow;
-
     [Header("Device Management")]
     [SerializeField] private Button DeviceManagementWindowButton;
     [SerializeField] private GameObject DeviceManagementWindow;
 
+    [Header("Content Management")]
+    [SerializeField] private Button ContentManagementWindowButton;
+    [SerializeField] private GameObject ContentManagementWindow;
+
+    [Header("Content Action")]
+    [SerializeField] private Button ContentActionWindowButton;
+    [SerializeField] private GameObject ContentActionWindow;
+
+
     private void Awake()
     {
+        ContentActionWindowButton.onClick.AddListener(ContentActionWindowOpen);
         ContentManagementWindowButton.onClick.AddListener(ContentManagementWindowOpen);
         DeviceManagementWindowButton.onClick.AddListener(DeviceManagementWindowOpen);
     }
 
     private void Start()
     {
-        ContentManagementWindowOpen();
+        ContentActionWindowOpen();
     }
 
     private void OnDestroy()
     {
+        ContentActionWindowButton.onClick?.RemoveListener(ContentActionWindowOpen);
         ContentManagementWindowButton.onClick?.RemoveListener(ContentManagementWindowOpen);
         DeviceManagementWindowButton.onClick?.RemoveListener(DeviceManagementWindowOpen);
     }
 
+    private void DeviceManagementWindowOpen()
+    {
+        ContentActionWindow.SetActive(false);
+        ContentManagementWindow.SetActive(false);
+        DeviceManagementWindow.SetActive(true);
+    }
+
     private void ContentManagementWindowOpen()
     {
+        ContentActionWindow.SetActive(false);
         ContentManagementWindow.SetActive(true);
         DeviceManagementWindow.SetActive(false);
     }
 
-    private void DeviceManagementWindowOpen()
+    private void ContentActionWindowOpen()
     {
+        ContentActionWindow.SetActive(true);
         ContentManagementWindow.SetActive(false);
-        DeviceManagementWindow.SetActive(true);
+        DeviceManagementWindow.SetActive(false);
     }
 }
